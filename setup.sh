9baa7cee7dda8973e546cae6210e692ae37cc307
Bryan Smith <bry-guy@github.com>
2021-09-27 16:11:32 -0700

    Fix typo.

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ ln -s $(pwd)/.aliases $HOME/.aliases
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
-sudo mv squashfs-root /usr/local/ && sudo ln -s /usr/local/squash-fsroot/AppRun /usr/bin/nvim
+sudo mv squashfs-root /usr/local/ 
+sudo ln -s /usr/local/squashfs-root/AppRun /usr/bin/nvim
 
 sudo chsh -s "$(which zsh)" "$(whoami)"
