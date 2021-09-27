d0618b1cc486244cbdae49c270075da3a52ad939
Bryan Smith <bry-guy@github.com>
2021-09-27 16:37:15 -0700

    Add empty init.vim.

--- setup.sh
+++ setup.sh
@@ -7,14 +7,18 @@ set -x
 ## remove codespaces built-ins
 rm -f $HOME/.zshrc
 
+## link the things
 ln -s $(pwd)/.zshrc $HOME/.zshrc
 ln -s $(pwd)/.aliases $HOME/.aliases
+ln -s $(pwd)/.vim
 
-## install neovim
+## neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 sudo mv squashfs-root /usr/local/ 
 sudo ln -s /usr/local/squashfs-root/AppRun /usr/bin/nvim
+
+
 
 sudo chsh -s "$(which zsh)" "$(whoami)"
