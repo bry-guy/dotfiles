b643415ed16e778fb0095aa2c670df7b80e10c58
Bryan Smith <bry-guy@github.com>
2021-09-27 15:13:21 -0700

    Link zshrc and aliases.

--- setup.sh
+++ setup.sh
@@ -4,4 +4,10 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
+## remove codespaces built-ins
+rm -f $HOME/.zshrc
+
+ln -s $(pwd)/.zshrc $HOME/.zshrc
+ln -s $(pwd)/.aliases $HOME/.aliases
+
 sudo chsh -s "$(which zsh)" "$(whoami)"
