7ac765daa0a25b0deb9b9f49cedef5d74e8f3903
Bryan Smith <bry-guy@github.com>
2021-09-28 14:16:21 -0700

    order matters

--- setup.sh
+++ setup.sh
@@ -9,8 +9,6 @@ rm -f $HOME/.zshrc
 
 ## setup
 mkdir $HOME/.config/nvim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 ## link the things
 ln -s $(pwd)/.zshrc $HOME/.zshrc
@@ -27,6 +25,10 @@ chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 sudo mv squashfs-root /usr/local/ 
 sudo ln -s /usr/local/squashfs-root/AppRun /usr/bin/nvim
+
+### install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 ### install plugins
 nvim --headless +PlugInstall +q
