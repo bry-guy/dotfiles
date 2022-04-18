c41a4e506baa6c0e4fde7c7f987e335b32aa0749
Bryan Smith <bryan@bry-guy.net>
2022-04-18 14:59:40 -0700

    Use targeted treesitter installs.

--- .config/nvim/_treesitter.lua
+++ .config/nvim/_treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",
+  ensure_installed = { "ruby", "python", "java", "vim", "go", "bash", "javascript", "typescript", "dockerfile", "lua", "html" },
   highlight = {
     enable = true,
   }
