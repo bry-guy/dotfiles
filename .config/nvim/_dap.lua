c4fc8d326f9be4b89aa788ea97641bf963aa2c3f
Bryan Smith <bryan@bry-guy.net>
2022-12-04 23:23:41 -0800

    Deprecate the ansible-installed neovim java dependencies.

--- .config/nvim/_dap.lua
+++ .config/nvim/_dap.lua
@@ -4,8 +4,9 @@ require("mason-nvim-dap").setup({
 	ensure_installed = {
 	  "javadbg",
 	  "javatest",
-	  "python"
+	  "python",
 	},
+	automatic_setup = {}
 })
 
 require("mason-nvim-dap").setup_handlers({
