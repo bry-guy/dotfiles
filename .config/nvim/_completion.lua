60d1bd4812dd0647a4c9072ebfd87a9b0e0c94cc
Bryan Smith <bryan@bry-guy.net>
2024-02-29 11:31:47 -0800

    Update karabiner hotkeys + alacrbitty. Make C-Space tmux prefix. Add tmate conf.

--- .config/nvim/_completion.lua
+++ .config/nvim/_completion.lua
@@ -5,15 +5,17 @@ local has_words_before = function()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
-local cmp = require'cmp'
+local cmp = require('cmp')
+local snippy = require('snippy')
 
 cmp.setup({
   completion = {
 		autocomplete = { require('cmp.types').cmp.TriggerEvent.TextChanged },
+  },
+  snippet = {
+	expand = function(args)
+	  require('snippy').expand_snippet(args.body)
+	end
   },
   mapping = {
     -- ['<C-Tab>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
@@ -21,6 +23,8 @@ cmp.setup({
 	["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
+	  elseif snippy.can_expand_or_advance() then
+		snippy.expand_or_advance()
       elseif has_words_before() then
         cmp.complete()
       else
@@ -31,6 +35,8 @@ cmp.setup({
     ["<S-Tab>"] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item()
+	  elseif snippy.can_jump(-1) then
+        snippy.previous()
       end
     end, { "i", "s" }),
     ['<C-s>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
@@ -38,7 +44,7 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), 
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
--- .config/nvim/_go.lua
+++ .config/nvim/_go.lua
@@ -4,14 +4,51 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     -- Save the current buffer number
     local bufnr = vim.api.nvim_get_current_buf()
 
+	-- Save the current cursor position
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+
     -- Run `go fmt` on the current file
     vim.api.nvim_command('silent !go fmt %')
 
     -- Reload the buffer in case `go fmt` made changes
     vim.api.nvim_command('e!')
 
+	-- Restore the cursor position
+    vim.api.nvim_win_set_cursor(0, cursor_pos)
+
     -- Set the buffer as unmodified (since `go fmt` has saved it)
     vim.api.nvim_buf_set_option(bufnr, 'modified', false)
   end,
 })
+
+
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   pattern = {"*.go"},
+--   callback = function()
+--     -- Save the current buffer number
+--     local bufnr = vim.api.nvim_get_current_buf()
+
+--     -- Save the current cursor position
+--     local cursor_pos = vim.api.nvim_win_get_cursor(0)
+
+--     -- Define the command to run `go fmt` on the current file
+--     local cmd = "go fmt " .. vim.api.nvim_buf_get_name(bufnr)
+
+--     -- Run `go fmt` asynchronously
+--     vim.fn.jobstart(cmd, {
+--       on_exit = function(_, exit_code)
+--         if exit_code == 0 then
+--           -- Reload the buffer in case `go fmt` made changes, avoiding cursor jump
+--           vim.api.nvim_buf_call(bufnr, function()
+--             vim.cmd('silent! e!')
+--             -- Restore the cursor position
+--             vim.api.nvim_win_set_cursor(0, cursor_pos)
+--             -- Set the buffer as unmodified (since `go fmt` has saved it)
+--             vim.api.nvim_buf_set_option(bufnr, 'modified', false)
+--           end)
+--         end
+--       end
+--     })
+--   end,
+-- })
 
